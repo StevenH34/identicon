afa21bd8a82fc5a37d46f91eb6c194d178ad5cee
Steven Hedges <steven.hedges34@gmail.com>
2024-09-13 23:54:16 -0400

    Add struct and mirror the rows

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,24 +3,35 @@ defmodule Identicon do
   Documentation for `Identicon`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-
   def main(input) do
     input
     |> hash_input_string
+    |> pick_color
+    |> build_grid
   end
 
   def hash_input_string(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail] } = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+    # [r ,g ,b]
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = _image) do
+    hex
+    |> Enum.chunk_every(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+    # row = [a, b, c]
+    [first, second | _tail] = row   # [a, b]
+    row ++ [second, first]          # row = [a, b, c, b, a]
   end
 
 end
